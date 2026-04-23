8e7f3e2b19724de556ad71c8075c7174e75af6cd
Crist <Crist@F16.bbrouter>
2026-04-22 21:34:12 -0600

    second

---
+++ src/main/java/com/mycompany/lab2clucho/Lab2Clucho.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab2clucho;
+
+/**
+ *
+ * @author umg
+ */
+public class Lab2Clucho {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
